84ae2f8eb696fe7e26f77ef08ed8974ad38171fc
wrvsrx <wrvsrx@outlook.com>
2026-06-18 14:10:01 +0800

    chore(dev): enable documentChanges for nvim

--- dev/nvim.lua
+++ dev/nvim.lua
@@ -1,7 +1,11 @@
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities.workspace.workspaceEdit.documentChanges = true
+
 vim.lsp.config['djot-language-server'] = {
   cmd = { './target/debug/djot-ls' },
   filetypes = { 'djot' },
   root_dir = vim.fs.root(0, { '.git' }),
+  capabilities = capabilities,
 }
 
 vim.lsp.enable('djot-language-server')
